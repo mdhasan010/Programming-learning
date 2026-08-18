eaaf571dc18f8426bce922b387fda6a630bfbfc3
Md. Nayeem Hasan <mdnayeemhasan010@gmail.com>
2026-08-18 10:17:12 +0600

    Python f-string

---
+++ Python/Basic/Input/input.py
@@ -0,0 +1,19 @@
+name= input("inter your name: ")
+print(name)
+
+
+age=int(input("inter your age: "))
+print(age)
+
+# multiple input
+
+name= input("your name: ")
+age=int(input("your age: "))
+print(name)
+print(age)
+
+#same line in the multiple value
+
+name, age = input("inter your name and age"). split()
+print(name)
+print(age)
---
+++ Python/Basic/f string/f-string.py
@@ -0,0 +1,12 @@
+name= "Nayeem"
+age=23
+
+print(f"my name is: {name} and my age: {age}")
+
+
+
+name=input("your name: ")
+age=input("yor age: ")
+
+print(f"Hello {name}")
+print(f"your age {age}")
