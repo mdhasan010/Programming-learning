c9e7f3809cd4a18742e60a09d60ef98834b461a0
Md. Nayeem Hasan <mdnayeemhasan010@gmail.com>
2026-08-19 10:34:59 +0600

    Python operoters

---
+++ Python/Basic/Python operator/operators.py
@@ -0,0 +1,32 @@
+# 1. Arithmetic Operator
+
+
+#Addition
+
+n1= 10
+n2= 20
+print(n1+n2)
+
+# - Subtraction
+
+n1= 10
+n2= 20
+print(n1-n2)
+
+# * Multiplication
+
+n1= 10
+n2= 20
+print(n1*n2)
+
+# Division
+
+n1= 10
+n2= 20
+print(n1 / n2)
+
+#Floor Division
+
+n1= 10
+n2= 20
+print(n1 // n2)
